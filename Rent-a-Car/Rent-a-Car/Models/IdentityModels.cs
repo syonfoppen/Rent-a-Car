8e3dce3945ff0d3eaac68990ca42c3e1229457e3
Syon Foppen <ikbensyon@gmail.com>
2019-11-22 13:22:10 +0100

    First project setup

---
+++ Rent-a-Car/Rent-a-Car/Models/IdentityModels.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Rent_a_Car.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        //persoonlijke data
+        public string Voornaam { get; set; }
+        public string Tussenvoegsel { get; set; }
+        public string Achternaam { get; set; }
+        public DateTime Geboortedatum { get; set; }
+
+
+        //Adres
+        public string Adres { get; set; }
+        public int Huisnummer { get; set; }
+        public string Toevoeging { get; set; }
+        public string PostCode { get; set; }
+        public string Plaats { get; set; }
+        public string Provincie { get; set; }
+        public string Land { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
