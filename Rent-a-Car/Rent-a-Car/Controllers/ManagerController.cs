0a1d63e85600f1e8627686000f3e79f75b98c9a7
Job van der Sluis <52531941+JobSluis@users.noreply.github.com>
2019-11-26 11:31:38 +0100

    Create manager controller and index

---
+++ Rent-a-Car/Rent-a-Car/Controllers/ManagerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rent_a_Car.Controllers
+{
+    [Authorize(Roles = "Manager")]
+    public class ManagerController : Controller
+    {
+        // GET: Manager
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
